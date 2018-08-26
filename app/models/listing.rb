459728d75b9b7e636f36fd58cbd74b501ab3a490
Susumu <sumura80@gmail.com>
2018-08-26 19:37:02 +0900

    added carrierwave

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,14 +2,15 @@ class Listing < ApplicationRecord
 	belongs_to :user
 	has_many :reviews
 
-	 mount_uploaders :image, ImageUploader
-  
+	 #mount_uploaders :image, ImageUploader
+  mount_uploader :image, ImageUploader
 
-  searchkick
+ 
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
-  validates :image_file_name, presence: true
+  #validates :image_file_name, presence: true
+  validates :image, presence: true
   validates :product_no, presence: true
 
 end
